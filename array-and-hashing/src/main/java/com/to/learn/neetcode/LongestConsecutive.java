85fa6649b5a033b2992e2400623348225094444e
Tam Tran <thtt116@me.com>
2022-07-10 23:24:38 -0500

    add longest consecutive

---
+++ array-and-hashing/src/main/java/com/to/learn/neetcode/LongestConsecutive.java
@@ -0,0 +1,52 @@
+package com.to.learn.neetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Given an unsorted array of integers nums, return the length of the longest
+ * consecutive elements sequence.
+ * 
+ * You must write an algorithm that runs in O(n) time.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [100,4,200,1,3,2]
+ * Output: 4
+ * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4].
+ * Therefore its length is 4.
+ * Example 2:
+ * 
+ * Input: nums = [0,3,7,2,5,8,4,6,0,1]
+ * Output: 9
+ * 
+ * 
+ * Constraints:
+ * 
+ * 0 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ */
+public class LongestConsecutive {
+
+    public static int longestConsecutive(int[] nums) {
+        Arrays.sort(nums);
+        // create int to store the count
+        int countLongest = 1;
+        // list to store count history
+        List<Integer> countList = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i < nums.length - 1 && nums[i] + 1 == nums[i + 1]) {
+                countLongest++;
+            } else {
+                countList.add(countLongest);
+                countLongest = 1;
+            }
+        }
+        // Utilize collections
+        return Collections.max(countList);
+    }
+}
---
+++ array-and-hashing/src/test/java/com/to/learn/neetcode/LongestConsecutiveTest.java
@@ -0,0 +1,13 @@
+package com.to.learn.neetcode;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LongestConsecutiveTest {
+    @Test
+    public void testLongestConsecutive() {
+        assertEquals(4, LongestConsecutive.longestConsecutive(new int[] { 100, 4, 200, 1, 3, 2 }));
+        assertEquals(9, LongestConsecutive.longestConsecutive(new int[] { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 }));
+    }
+}
